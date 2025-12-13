3682dfe68fd9253591e1b6f54b87dbd9de8368b7
Cosmo <150472944+Cosmos255@users.noreply.github.com>
2025-12-13 20:53:52 +0200

    Lovely

--- parser.h
+++ parser.h
@@ -4,22 +4,6 @@
 #include <vector>
 #include <fstream>
 
-struct File{
-    std::ifstream in;
-
-    File(){
-        std::cerr<<"Error no input file";
-    };
-    File(const std::string &input_f){
-        in.open(input_f);
-        if(!in){
-            std::cerr << "Error could not open file";
-        }
-    }
-    
-};
-
-
 
 using type_t = enum
 {
@@ -41,9 +25,6 @@ struct Token
     type_t type;
     std::string value;
 
-    Token getNextToken(){
-        //File IN >>value;
-    };
 };
 
 std::vector<Token> Tokens;
@@ -51,11 +32,72 @@ std::vector<Token> Tokens;
 
 class Lexer {
 public:
-    Lexer(const std::string& input) : input(input), pos(0){}
-    Token getNextToken();
+    Lexer(const std::string& filename): in(filename),line(1),col(1){
+        if(!in){
+            std::cerr<<"No existing files found";
+        }
+    }
+
+    void Tokenizer(){
+        Token token_t;
+
+        switch (peek()){
+        case '{': Tokens.push_back({L_Brace, "{"}); break;
+        case '}': Tokens.push_back({R_Brace, "}"}); break;
+        case '"': Tokens.push_back({STRING, readString()}); break;
+        case ':': Tokens.push_back({Colon, ":"}); break;
+        case ',': Tokens.push_back({Comma, ","}); break;
+        case ' ': skipWL(); break;
+        case 't': Tokens.push_back({True, "true"}); break;
+        case 'f': Tokens.push_back({False, "false"}); break;
+        case '-': Tokens.push_back({NUMBER, readNumber()}); break;
+        default:
+            Tokens.push_back({NUMBER, readNumber()});
+        }
+
+        
+
+
+
+        if(!in.eof()) Tokenizer;
+    }
+
+
+
+
 private:
-    std::string input;
-    size_t pos;
+    std::ifstream in;
+    int line, col;
+
+    char peek(){
+        char c;
+        c=in.peek();
+        return c;
+    };
+
+    char advance(){
+        
+    };
+
+    std::string readString(){
+        in.get();
+        std::string data;
+        std::getline(in, data, '"');
+        return data;
+    }
+
+    std::string readNumber(){
+        char n = peek();
+        if(isdigit(n) || n == '-' || n=='E' || n=='e' || n=='.' || n=='+'){
+            
+        }
+    }
+
+    void skipWL(){
+        if(peek()==' '){
+            in.get();
+        }
+    }
 
 };
 
