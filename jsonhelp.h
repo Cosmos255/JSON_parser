738c5e195bcb7c2486ab4c70b8709a5c76269152
Cosmo <150472944+Cosmos255@users.noreply.github.com>
2025-12-27 22:20:10 +0200

    Made somne modification to accesing

--- jsonhelp.h
+++ jsonhelp.h
@@ -4,32 +4,16 @@
 #include <stdexcept>
 
 
-JsonObject& asObject(JsonValue& v) {
-    if (auto p = std::get_if<JsonObject>(&v.data)) return *p;
-    throw std::runtime_error("JsonValue is not an object");
+template <typename T>
+T& get(JsonValue& v) {
+    if (auto* p = std::get_if<T>(&v.data))
+        return *p;
+    throw std::runtime_error("wrong type");
 }
 
-JsonArray& asArray(JsonValue& v) {
-    if (auto p = std::get_if<JsonArray>(&v.data)) return *p;
-    throw std::runtime_error("JsonValue is not an array");
-}
-
-std::string& asString(JsonValue& v) {
-    if (auto p = std::get_if<std::string>(&v.data)) return *p;
-    throw std::runtime_error("JsonValue is not a string");
-}
-
-double asDouble(JsonValue& v) {
-    if (auto p = std::get_if<double>(&v.data)) return *p;
-    throw std::runtime_error("JsonValue is not a number");
-}
-
-int asInt(JsonValue& v) {
-    return static_cast<int>(asDouble(v));
-}
 
 JsonValue& getKey(JsonObject& obj, const std::string& key) {
-    auto it = obj.find(key);
-    if (it == obj.end()) throw std::runtime_error("Key \"" + key + "\" not found");
-    return it->second;
+    auto value = obj.find(key);
+    if(value==obj.end()) throw std::runtime_error("Key " + key + "not found");
+    return value->second;
 }
--- main.cpp
+++ main.cpp
@@ -3,22 +3,18 @@
 #include <vector>
 #include <string>
 #include "json.h"
-#include "jsonhelp.h"
 
 int main(){
 
    JsonValue root = json::parseJson("test.json");
-   JsonObject& student = asObject(root); 
+   auto &student = json::get<JsonObject>(root); 
 
-   std::string name = asString(getKey(student, "name"));
-   int age = std::stoi(asString(getKey(student, "age")));
-   bool active = std::get<bool>(getKey(student, "active").data);
+   auto name = json::get<std::string>(student["name"]);
+
 
 
    std::cout << "Name: " << name << "\n";
-   std::cout << "Age: " << age << "\n";
-   std::cout << "Active: " << active << "\n";
-
+   
    /*
    Finished the lexer/parse it worjs but accesing is a pain
    so to do:
